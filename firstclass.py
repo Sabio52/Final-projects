6ef2134d69ec8a6129d39aaa7c3885477d096427
Sabio52 <sabitibne5286@gmail.com>
2025-06-21 12:46:34 +0600

    Add files via upload

--- firstclass.py
+++ firstclass.py
@@ -1,4 +1,3 @@
-print('I am Sabit Ibne Hosaan,')
-print('I am a class 8 student of CPSC.')
-print('I am 14 years old.')
-print('I live in Dhaka, Bangladesh.')
+name = "Sabit"
+age = "14"
+print("Hello, my name is", name, "and I am", age, "years old.")
