cf03e629a35fe6dfe0fa2af4c64888a36d932ac8
Sabio52 <sabitibne5286@gmail.com>
2025-07-12 12:24:35 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,18 @@
+first = ["Apple", "Banana", "Cherry", "Mango", "Blueberry"]
+
+print("length of first:", len(first))
+print("first fruit:", first[0])
+print("last fruit:", first[-1])
+first.append("Orange")
+print("Updated list:", first)
+first.remove("Banana")
+print("After removing Banana:", first)
+first.sort()
+print("Sorted list:", first)
+first.reverse()
+print("Reversed list:", first)
+print("multiplication of list:", first * 2)
+first.pop(2)
+print("After popping:", first)
+first.clear()
+print("After clearing the list:", first)
